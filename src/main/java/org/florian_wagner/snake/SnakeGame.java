ddbdaf7dfc5b233419427023a9bfe8c4cb48a657
nexotekHD <florianwagner.bremen@googlemail.com>
2017-04-03 15:21:42 +0200

    first commit

---
+++ src/main/java/org/florian_wagner/snake/SnakeGame.java
@@ -0,0 +1,20 @@
+package org.florian_wagner.snake;
+
+/**
+ * Created by Florian on 03.04.2017.
+ */
+public class SnakeGame {
+
+
+
+    public static void main(String[] args)
+    {
+        new SnakeGame().init();
+    }
+
+    public void init()
+    {
+
+    }
+
+}
